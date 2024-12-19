c8edc96c2d4de4d432aeed130624c1c29788c4a3
jacek-jonca <jacek@jonca.org>
2024-12-19 05:07:27 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""An example of showing geographic data."""
+"""An example of showing geographic enrollment data."""
 
 import os
 
@@ -24,33 +24,30 @@ import pydeck as pdk
 import streamlit as st
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
-
+st.set_page_config(layout="wide", page_title="Enrollment Sources", page_icon=":taxi:")
 
 # LOAD DATA ONCE
 @st.cache_resource
 def load_data():
-    path = "uber-raw-data-sep14.csv.gz"
+    path = "data.csv"
     if not os.path.isfile(path):
-        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+        path = f"https://github.com/jacek-jonca/interactive-enrollment-sources/raw/main/{path}"
 
     data = pd.read_csv(
         path,
-        nrows=100000,  # approx. 10% of data
+        nrows=100000,  # there shouldn't be more than 100k records
         names=[
-            "date/time",
             "lat",
             "lon",
         ],  # specify names directly since they don't change
         skiprows=1,  # don't read header since names specified directly
-        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
-        parse_dates=[
-            "date/time"
-        ],  # set as datetime instead of converting after the fact
+        usecols=[0, 1],  # only two columns so tobe safe loading two
+#        parse_dates=[
+#            "date/time"
+#        ],  # set as datetime instead of converting after the fact
     )
 
     return data
-
 
 # FUNCTION FOR AIRPORT MAPS
 def map(data, lat, lon, zoom):
@@ -78,30 +75,19 @@ def map(data, lat, lon, zoom):
         )
     )
 
-
-# FILTER DATA FOR A SPECIFIC HOUR, CACHE
-@st.cache_data
-def filterdata(df, hour_selected):
-    return df[df["date/time"].dt.hour == hour_selected]
-
-
 # CALCULATE MIDPOINT FOR GIVEN SET OF DATA
 @st.cache_data
 def mpoint(lat, lon):
     return (np.average(lat), np.average(lon))
 
-
 # FILTER DATA BY HOUR
 @st.cache_data
-def histdata(df, hr):
+def histdata(df):
     filtered = data[
         (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
     ]
-
     hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
-
     return pd.DataFrame({"minute": range(60), "pickups": hist})
-
 
 # STREAMLIT APP LAYOUT
 data = load_data()
@@ -109,70 +95,39 @@ data = load_data()
 # LAYING OUT THE TOP SECTION OF THE APP
 row1_1, row1_2 = st.columns((2, 3))
 
-# SEE IF THERE'S A QUERY PARAM IN THE URL (e.g. ?pickup_hour=2)
-# THIS ALLOWS YOU TO PASS A STATEFUL URL TO SOMEONE WITH A SPECIFIC HOUR SELECTED,
-# E.G. https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/main?pickup_hour=2
-if not st.session_state.get("url_synced", False):
-    try:
-        pickup_hour = int(st.query_params["pickup_hour"])
-        st.session_state["pickup_hour"] = pickup_hour
-        st.session_state["url_synced"] = True
-    except KeyError:
-        pass
-
-
-# IF THE SLIDER CHANGES, UPDATE THE QUERY PARAM
-def update_query_params():
-    hour_selected = st.session_state["pickup_hour"]
-    st.query_params["pickup_hour"] = hour_selected
-
-
 with row1_1:
-    st.title("NYC Uber Ridesharing Data")
-    hour_selected = st.slider(
-        "Select hour of pickup", 0, 23, key="pickup_hour", on_change=update_query_params
-    )
-
+    st.title("Enrollment Sources Data")
 
 with row1_2:
     st.write(
         """
     ##
-    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
-    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
+    Visualizing enrollments
     """
     )
 
 # LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
+row2_1, row2_2 = st.columns((2, 2))
 
 # SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
-la_guardia = [40.7900, -73.8700]
-jfk = [40.6650, -73.7821]
-newark = [40.7090, -74.1805]
+texas = [31.9686, -99.9018]
+houston = [29.7601, -95.3701]
+#newark = [40.7090, -74.1805]
 zoom_level = 12
 midpoint = mpoint(data["lat"], data["lon"])
 
 with row2_1:
     st.write(
-        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+        f"""**All Texas"""
     )
-    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
+    map(data, midpoint[0], midpoint[1], 11)
 
 with row2_2:
-    st.write("**La Guardia Airport**")
-    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
-
-with row2_3:
-    st.write("**JFK Airport**")
-    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
-
-with row2_4:
-    st.write("**Newark Airport**")
-    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
+    st.write("**Houston**")
+    map(data, houston[0], houston[1], zoom_level)
 
 # CALCULATING DATA FOR THE HISTOGRAM
-chart_data = histdata(data, hour_selected)
+chart_data = histdata(data)
 
 # LAYING OUT THE HISTOGRAM SECTION
 st.write(
