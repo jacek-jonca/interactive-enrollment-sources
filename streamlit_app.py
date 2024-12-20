36e2da1ab83cd93b792e0f354fc0d093cc26bc10
jacek-jonca <jacek@jonca.org>
2024-12-20 17:06:20 +0000

    simplification, single braces

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ import pydeck as pdk
 import streamlit as st
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="Enrollment Sources", page_icon=":taxi:")
+st.set_page_config(layout="wide", page_title="Enrollment Sources", page_icon=":student:")
 
 # LOAD DATA ONCE
 @st.cache_resource
@@ -33,40 +33,38 @@ def load_data():
 
     data = pd.read_csv(
         path,
-#        nrows=100000,  # there shouldn't be more than 100k records
         names=[
-            "lat",
             "lon",
+            "lat",
         ],  # specify names directly since they don't change
         skiprows=1,  # don't read header since names specified directly
         usecols=[0, 1],  # only two columns so tobe safe loading two
-#        parse_dates=[
-#            "date/time"
-#        ],  # set as datetime instead of converting after the fact
     )
     return data
 
 # FUNCTION FOR MAPS
-def map(data, lat, lon, zoom):
+def map(data, lon, lat, zoom):
     st.write(
         pdk.Deck(
             map_style="mapbox://styles/mapbox/light-v9",
             initial_view_state={
-                "latitude": lat,
                 "longitude": lon,
+                "latitude": lat,
                 "zoom": zoom,
                 "pitch": 50,
             },
             layers=[
                 pdk.Layer(
-                    "HexagonLayer",
+                    'HexagonLayer',
                     data=data,
-                    get_position=["lon", "lat"],
+                    get_position=['lon', 'lat'],
                     radius=100,
                     elevation_scale=4,
                     elevation_range=[0, 1000],
+                    auto_highlight=True,
                     pickable=True,
                     extruded=True,
+                    coverage=1,
                 ),
             ],
         )
@@ -74,23 +72,14 @@ def map(data, lat, lon, zoom):
 
 # CALCULATE MIDPOINT FOR GIVEN SET OF DATA
 @st.cache_data
-def mpoint(lat, lon):
-    return (np.average(lat), np.average(lon))
-
-# FILTER DATA BY HOUR
-#@st.cache_data
-#def histdata(df):
-#    filtered = data[
-#        (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
-#    ]
-#    hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
-#    return pd.DataFrame({"minute": range(60), "pickups": hist})
+def mpoint(lon, lat):
+    return (np.average(lon), np.average(lat))
 
 # STREAMLIT APP LAYOUT
 data = load_data()
 
 # LAYING OUT THE TOP SECTION OF THE APP
-row1_1, row1_2 = st.columns((2, 3))
+row1_1, row1_2 = st.columns((3, 2))
 
 with row1_1:
     st.title("Enrollment Sources Data")
@@ -107,13 +96,13 @@ with row1_2:
 row2_1, row2_2 = st.columns((2, 2))
 
 # SETTING GEOGRAPHIC CENTERS
-texas = [31.391533, -99.17065]
-houston = [29.7601, -95.3701]
-dallas = [32.7079, -96.9209]
-austin = [30.274722, -97.740556]
-sanAntonio = [29.424349, -98.491142]
+texas = [-99.17065, 31.391533]
+houston = [-95.3701, 29.7601]
+dallas = [-96.9209, 32.7079]
+austin = [-97.740556, 30.274722]
+sanAntonio = [-98.491142, 29.424349]
 zoom_level = 9
-midpoint = mpoint(data["lat"], data["lon"])
+midpoint = mpoint(data["lon"], data["lat"])
 
 with row2_1:
     st.write(
