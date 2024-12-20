90556e735fa60ae689527f60fdbe6ef5f3a43ec3
jacek-jonca <jacek@jonca.org>
2024-12-20 21:18:12 +0000

    row layout changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,13 +97,15 @@ locations = {
 midpoint = calculate_midpoint(data)
 
 # LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-row1_1, row1_2, row2_1, row2_2, row3_1, row3_2  = st.columns((2, 2, 2, 2, 2, 2))
+#row1_1, row1_2, row2_1, row2_2, row3_1, row3_2  = st.columns((2, 2, 2, 2, 2, 2))
 
 with st.container():
     render_map(data, midpoint[0], midpoint[1], 7, "Geographic Midpoint")
 
 with st.container():
     render_map(data, -95.3701, 29.7601, 9, "Houston")
+
+row2_1, row2_2, row3_1, row3_2  = st.columns((2, 2, 2, 2))
 
 with row2_1:
     render_map(data, -96.9209, 32.7079, 9, "Dallas")
